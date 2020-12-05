0d102f35a92a25b6734f8b0a0990e67c3dc22073
dablenparty <57240002+dablenparty@users.noreply.github.com>
2020-12-04 22:32:57 -0500

    Added status indication

--- src/download.js
+++ src/download.js
@@ -4,15 +4,9 @@ const ffmpeg = require("fluent-ffmpeg");
 const Store = require("./config.js");
 const electron = require('electron');
 const path = require('path');
+const fs = require('fs');
 
 const downloadBtn = document.getElementById("download-btn");
-
-const store = new Store({
-    configName: "user-settings",
-    defaults: {
-        savePath: electron.remote.app.getPath("downloads"),
-    },
-});
 
 let removeIllegalChars = (filepath) => {
   const illegal = /[\\/:*?\"<>|]/g;
@@ -48,7 +42,36 @@ let createErrorElement = (error) => {
   }
 };
 
+var saveFile = async (url, file_path) => {
+  var stream = ytdl(url, { filter: "audioonly" });
+  ffmpeg(stream).save(file_path);
+  var size = 0, mtime = 0;
+  while (true) {
+    try {
+      var stats = fs.statSync(file_path);
+    } catch (err) {
+      await new Promise(r => setTimeout(r, 500));
+      continue;
+    }
+    if (!(stats.size == size || stats.mtime == mtime)) {
+      size = stats.size;
+      mtime = stats.mtime;
+      await new Promise(r => setTimeout(r, 500));
+      continue;
+    }
+    console.log("done");
+    break;
+  }
+  return 0;
+}
+
 const download = () => {
+  const store = new Store({
+    configName: "user-settings",
+    defaults: {
+        savePath: electron.remote.app.getPath("downloads"),
+      },
+  });
   let status = document.getElementById('status');
   let url = document.getElementById("url-box").value;
   let file_name = removeIllegalChars(document.getElementById("name-box").value);
@@ -58,12 +81,9 @@ const download = () => {
   status.innerHTML = "<i>Downloading...</i>"
   
   try {
-    var stream = ytdl(url, { filter: "audioonly" });
-    ffmpeg(stream).save(file_path);
-    document
-      .getElementsByClassName("downloader")[0]
-      .removeChild(document.getElementById("error-out"));
-    status.innerHTML = "<i>Done!</i>"
+    saveFile(url, file_path).then(() => {
+      status.innerHTML = "<i>Done!</i>";
+    });
   } catch (error) {
     status.innerHTML = "<i>Error</i>"
     console.error(error);
