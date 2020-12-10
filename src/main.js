bc29de6fd971375539d27c99457c007e3985e559
dablenparty <57240002+dablenparty@users.noreply.github.com>
2020-12-10 13:35:01 -0500

    Revert "Doesn't open dev tools now"
    This reverts commit a18b46d66081704b8f6ecd5ee6c7a19e16fc238f.

--- src/main.js
+++ src/main.js
@@ -65,7 +65,26 @@ const createWindow = () => {
             settingsWindow.loadURL(path.join("file://", __dirname, 'settings.html'));
             settingsWindow.show()
           }
-        }
+        },
+        {
+          label: 'Quit',
+          accelerator: "CmdOrCtrl+Q",
+          click() {
+            app.quit();
+          }
+        },
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
       ]
     }
   ])
